5eb0bcccf68ef7d7e7b140cca9264c711b4fd814
Joshua Hodosh <josh.hodosh@ll.mit.edu>
2014-04-11 15:07:04 -0400

    track dups and reads

--- qemu/panda_plugins/syscalls/fdtracker.cpp
+++ qemu/panda_plugins/syscalls/fdtracker.cpp
@@ -25,6 +25,7 @@ PANDAENDCOMMENT */
 #include <string>
 #include "weak_callbacks.hpp"
 #include "syscalls.hpp"
+#include <iostream>
 
 const target_ulong NULL_FD = 0;
 
@@ -39,6 +40,12 @@ class OpenCallbackData : public CallbackData {
 public:
     string path;
     target_ulong base_fd;
+};
+
+class DupCallbackData: public CallbackData {
+public:
+    target_ulong old_fd;
+    target_ulong new_fd;
 };
 
 static target_ulong calc_retaddr(CPUState* env, target_ulong pc){
@@ -60,16 +67,6 @@ static target_ulong calc_retaddr(CPUState* env, target_ulong pc){
 #endif
 }
 
-//mkdirs
-void call_sys_mkdirat_callback(CPUState* env,target_ulong pc,uint32_t dfd,std::string pathname,uint32_t mode) { 
-    
-}
-
-void call_sys_mkdir_callback(CPUState* env,target_ulong pc,std::string pathname,uint32_t mode) { 
-    
-}
-//opens
-
 static void open_callback(CallbackData* opaque, CPUState* env, target_asid asid){
     OpenCallbackData* data = dynamic_cast<OpenCallbackData*>(opaque);
     if(!data){
@@ -86,6 +83,22 @@ static void open_callback(CallbackData* opaque, CPUState* env, target_asid asid)
     mymap[get_return_val(env)] = dirname;
 }
 
+//mkdirs
+void call_sys_mkdirat_callback(CPUState* env,target_ulong pc,uint32_t dfd,std::string pathname,uint32_t mode) { 
+    OpenCallbackData* data = new OpenCallbackData;
+    data->path = pathname;
+    data->base_fd = NULL_FD;
+    appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, open_callback));
+}
+
+void call_sys_mkdir_callback(CPUState* env,target_ulong pc,std::string pathname,uint32_t mode) { 
+    OpenCallbackData* data = new OpenCallbackData;
+    data->path = filename;
+    data->base_fd = dfd;
+    appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, open_callback));
+}
+//opens
+
 void call_sys_open_callback(CPUState *env, target_ulong pc, std::string filename,uint32_t flags,uint32_t mode){
     OpenCallbackData* data = new OpenCallbackData;
     data->path = filename;
@@ -98,10 +111,71 @@ void call_sys_openat_callback(CPUState* env,target_ulong pc,uint32_t dfd,std::st
     data->path = filename;
     data->base_fd = dfd;
     appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, open_callback));
-    
+}
+
+static void dup_callback(CallbackData* opaque, CPUState* env, target_asid asid){
+    DupCallbackData* data = dynamic_cast<DupCallbackData*>(opaque);
+    if(!data){
+        fprintf(stderr, "oops\n");
+        return;
+    }
+    target_ulong new_fd;
+    if(data->new_fd != NULL_FD){
+        new_fd = data->new_fd;
+    }else{
+        new_fd = get_return_val(env);
+    }
+    asid_to_fds[asid][new_fd] = asid_to_fds[asid][old_fd];
 }
 
 // dups
+void call_sys_dup_callback(CPUState* env,target_ulong pc,uint32_t fildes) {
+    DupCallbackData* data = new DupCallbackData;
+    data->old_fd = fildes;
+    data->new_fd = NULL_FD;
+    appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, dup_callback));
+    
+}
+void call_sys_dup2_callback(CPUState* env,target_ulong pc,uint32_t oldfd,uint32_t newfd) { 
+    target_asid asid = get_asid(env, pc);
+    asid_to_fds[asid][newfd] = asid_to_fds[asid][oldfd];
+    return;
+    
+    DupCallbackData* data = new DupCallbackData;
+    data->old_fd = oldfd;
+    data->new_fd = newfd;
+    appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, dup_callback));
+    
+}
+void call_sys_dup3_callback(CPUState* env,target_ulong pc,uint32_t oldfd,uint32_t newfd,uint32_t flags) {
+    target_asid asid = get_asid(env, pc);
+    asid_to_fds[asid][newfd] = asid_to_fds[asid][oldfd];
+    return;
+    
+    DupCallbackData* data = new DupCallbackData;
+    data->old_fd = oldfd;
+    data->new_fd = newfd;
+    appendReturnPoint(ReturnPoint(calc_retaddr(env, pc), get_asid(env, pc), data, dup_callback));
+    
+}
 
 // close
+void call_sys_close_callback(CPUState* env,target_ulong pc,uint32_t fd) { }
 
+void call_sys_readahead_callback(CPUState* env,target_ulong pc,uint32_t fd,uint64_t offset,uint32_t count) { }
+
+void call_sys_read_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong buf,uint32_t count) { 
+    target_asid asid = get_asid(env, pc);
+    cout << "Reading from " << asid_to_fds[asid][fd];
+}
+void call_sys_readv_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong vec,uint32_t vlen) { 
+    target_asid asid = get_asid(env, pc);
+    cout << "Reading v from " << asid_to_fds[asid][fd];
+}
+void call_sys_pread64_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong buf,uint32_t count,uint64_t pos) {
+        target_asid asid = get_asid(env, pc);
+        cout << "Reading p64 from " << asid_to_fds[asid][fd];
+}
+void call_sys_write_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong buf,uint32_t count) { }
+void call_sys_pwrite64_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong buf,uint32_t count,uint64_t pos) { }
+void call_sys_writev_callback(CPUState* env,target_ulong pc,uint32_t fd,target_ulong vec,uint32_t vlen) { }
