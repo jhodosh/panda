50b0c63f27a86a84614a1912db2c34d474cb4730
Joshua Hodosh <josh.hodosh@ll.mit.edu>
2014-04-11 11:17:52 -0400

    syscalls: actually register callbacks

--- qemu/panda_plugins/syscalls/syscalls.cpp
+++ qemu/panda_plugins/syscalls/syscalls.cpp
@@ -352,7 +352,6 @@ bool init_plugin(void *self) {
         fprintf(stderr, "Couldn't open /scratch/syscalls.txt. Abort.\n");
         return false;
     }
-    else return true;
 
 // Don't bother if we're not on x86
 #if  defined(TARGET_I386) || defined(TARGET_ARM)
@@ -366,6 +365,7 @@ bool init_plugin(void *self) {
     panda_register_callback(self, PANDA_CB_BEFORE_BLOCK_EXEC, pcb);
 #endif
 
+    return true;
 }
 
 void uninit_plugin(void *self) {
