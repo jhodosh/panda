d715ce082f5f37d9cd80702fcb2d5c125ab72d23
Joshua Hodosh <josh.hodosh@ll.mit.edu>
2014-09-24 14:34:30 -0400

    fail on bad load

--- qemu/rr_log.c
+++ qemu/rr_log.c
@@ -1517,6 +1517,11 @@ int rr_do_begin_replay(const char *file_name_full, void *cpu_state) {
         plist->entry.before_loadvm();
     }
   snapshot_ret = load_vmstate_rr(name_buf);
+  if (0 != snapshot_ret){
+      // TODO: free rr_path and rr_name
+      printf("Failed to load snapshot for replay: %d\n", snapshot_ret);
+      return snapshot_ret;
+  }
   printf ("... done.\n");
   log_all_cpu_states();
 
@@ -1536,6 +1541,7 @@ int rr_do_begin_replay(const char *file_name_full, void *cpu_state) {
 
   //mz fill the queue!
   rr_fill_queue();
+  return snapshot_ret;
 #endif
 }
 
